15c55ddcfd94156744ef60ae7eea12fe48599db8
Synclair Wang <synclairwang11@yahoo.com>
2018-04-03 15:28:28 -0400

    CREATE READ AND DELETE UP

--- backend/books-api/src/main/java/com/example/booksapi/controllers/BooksController.java
+++ backend/books-api/src/main/java/com/example/booksapi/controllers/BooksController.java
@@ -3,8 +3,8 @@ package com.example.booksapi.controllers;
 import com.example.booksapi.models.Book;
 import com.example.booksapi.repositories.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BooksController {
@@ -15,5 +15,23 @@ public class BooksController {
     @GetMapping("/")
     public Iterable<Book> getAllUsers() {
         return booksRepository.findAll();
+    }
+
+    @GetMapping("/{book_id}")
+    public Book getBookById(@PathVariable Long book_id) {
+        Book selectedBook = booksRepository.findOne(book_id);
+        return selectedBook;
+    }
+
+    @PostMapping("/")
+    public void saveBook(@RequestBody Book book) {
+        booksRepository.save(book);
+    }
+
+    @DeleteMapping("/{book_id}")
+    public HttpStatus deleteBookById(@PathVariable Long book_id) throws Exception {
+        Book selectedBook = booksRepository.findOne(book_id);
+        booksRepository.delete(selectedBook);
+        return HttpStatus.OK;
     }
 }
